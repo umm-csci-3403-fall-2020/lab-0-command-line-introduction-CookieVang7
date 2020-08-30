0fc3485b293abb55d590441928f6f927919d41e4
Cookie Vang <vang2660@morris.umn.edu>
2020-08-30 13:22:38 -0500

    Added clarification comments to shellscript

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -6,7 +6,8 @@ tar -xzf NthPrime.tgz
 #navigate to NthPrime file
 cd NthPrime
 
-#compile NthPrime file
+#compiles and puts it into NthPrime file
 gcc main.c nth_prime.c -o NthPrime
 
+#Passes in the first command line argument
 ./NthPrime $1
