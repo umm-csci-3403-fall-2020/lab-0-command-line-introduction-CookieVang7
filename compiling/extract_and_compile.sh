eb7229879a41c5b2403b12a522eac040e6b2166e
Cookie Vang <vang2660@morris.umn.edu>
2020-08-30 12:28:15 -0500

    Finished script to pass all tests

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/bash
 
 #extract and uncompress the NthPrime tar file
-tar xvzf NthPrime.tgz 
+tar -xzf NthPrime.tgz
+
+#navigate to NthPrime file
+cd NthPrime
+
+#compile NthPrime file
+gcc main.c nth_prime.c -o NthPrime
+
+./NthPrime $1
